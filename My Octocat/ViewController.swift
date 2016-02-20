522b624eaff1934231ee9228d3812d0b5803a529
Daniele Vitali <daniele.vitali89@gmail.com>
2016-02-20 12:50:43 +0100

    First commit

---
+++ My Octocat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Octocat
+//
+//  Created by Daniele Vitali on 2/20/16.
+//  Copyright © 2016 Daniele Vitali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
