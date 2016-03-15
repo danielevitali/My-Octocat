d67fa7edf16d14645e9fea3409ea340ac0b1bddf
Daniele Vitali <daniele.vitali89@gmail.com>
2016-03-15 09:17:12 +0100

    feat: dynamic cells in user profile

---
+++ My Octocat/UserProfileCell.swift
@@ -0,0 +1,53 @@
+//
+//  ProfileCell.swift
+//  My Octocat
+//
+//  Created by Daniele Vitali on 3/13/16.
+//  Copyright © 2016 Daniele Vitali. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserProfileCell: UITableViewCell {
+    
+    @IBOutlet weak var imgAvatar: UIImageView!
+    @IBOutlet weak var lblName: UILabel!
+    @IBOutlet weak var lblUsername: UILabel!
+    @IBOutlet weak var lblMemberSince: UILabel!
+    @IBOutlet weak var vCity: UIView!
+    @IBOutlet weak var lblCity: UILabel!
+    @IBOutlet weak var vCompany: UIView!
+    @IBOutlet weak var lblCompany: UILabel!
+    @IBOutlet weak var vBio: UIView!
+    @IBOutlet weak var lblBio: UILabel!
+    
+    func showProfile(profile: Profile) {
+        lblName.text = profile.name
+        lblUsername.text = profile.username
+        lblMemberSince.text = "Member since \(NSDate.ISOStringFromDate(profile.creationDate))"
+        if let location = profile.location {
+            lblCity.text = location
+            vCity.hidden = false
+        } else {
+            vCity.hidden = true
+        }
+        if let company = profile.company {
+            lblCompany.text = company
+            vCompany.hidden = false
+        } else {
+            vCompany.hidden = true
+        }
+        if let bio = profile.bio {
+            lblBio.text = bio
+            vBio.hidden = false
+        } else {
+            vBio.hidden = true
+        }
+    }
+    
+    func showAvatar(image: NSData) {
+        imgAvatar.image = UIImage(data: image)
+    }
+    
+}
--- My Octocat/UserProfileViewController.swift
+++ My Octocat/UserProfileViewController.swift
@@ -11,27 +11,31 @@ import UIKit
 
 class UserProfileViewController: UIViewController, UserProfileViewContract, UITableViewDataSource, UITableViewDelegate {
     
+    private static let USER_PROFILE_CELL_IDENTIFIER = "UserProfileCell"
+    private static let USER_REPOSITORY_CELL_IDENTIFIER = "UserRepositoryCell"
+    
     @IBOutlet weak var aiLoading: UIActivityIndicatorView!
     @IBOutlet weak var tvUserData: UITableView!
     
     var presenter: UserProfilePresenterContract!
+    lazy var userProfileCell = UINib(nibName: UserProfileViewController.USER_PROFILE_CELL_IDENTIFIER, bundle: nil).instantiateWithOwner(nil, options: nil).first as! UserProfileCell
+    lazy var userRepositoryCell = UINib(nibName: UserProfileViewController.USER_REPOSITORY_CELL_IDENTIFIER, bundle: nil).instantiateWithOwner(nil, options: nil).first as! UserRepositoryCell
     
     override func viewDidLoad() {
         super.viewDidLoad()
         presenter.viewDidLoad()
     }
     
-    //TODO: show profile
     func showUserProfile(profile: Profile) {
         tvUserData.reloadData()
     }
     
     func showUserAvatar(image: NSData) {
-        let profileCell = tvUserData.cellForRowAtIndexPath(NSIndexPath(forItem: 0, inSection: 0)) as! ProfileCell
-        profileCell.showAvatar(image)
+        if let profileCell = tvUserData.cellForRowAtIndexPath(NSIndexPath(forItem: 0, inSection: 0)) as? UserProfileCell {
+            profileCell.showAvatar(image)
+        }
     }
     
-    //TODO: show repositories
     func showUserRepositories(repositories: [Repository]) {
         tvUserData.reloadData()
     }
@@ -57,13 +61,45 @@ class UserProfileViewController: UIViewController, UserProfileViewContract, UITa
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
-            let profileCell = tableView.dequeueReusableCellWithIdentifier("ProfileCell") as! ProfileCell
-            profileCell.showProfile(presenter.user.profile!)
-            return profileCell
+            var userProfileCell = tableView.dequeueReusableCellWithIdentifier(UserProfileViewController.USER_PROFILE_CELL_IDENTIFIER) as? UserProfileCell
+            if userProfileCell == nil {
+                tableView.registerNib(UINib.init(nibName: UserProfileViewController.USER_PROFILE_CELL_IDENTIFIER, bundle: nil), forCellReuseIdentifier: UserProfileViewController.USER_PROFILE_CELL_IDENTIFIER)
+                userProfileCell = tableView.dequeueReusableCellWithIdentifier(UserProfileViewController.USER_PROFILE_CELL_IDENTIFIER) as? UserProfileCell
+            }
+            return userProfileCell!
         }
-        let repositoryCell = tableView.dequeueReusableCellWithIdentifier("UserRepositoryCell") as! UserRepositoryCell
-        repositoryCell.showRepository(presenter.user.repositories![indexPath.row - 1])
-        return repositoryCell
+        var userRepositoryCell = tableView.dequeueReusableCellWithIdentifier(UserProfileViewController.USER_REPOSITORY_CELL_IDENTIFIER) as? UserRepositoryCell
+        if userRepositoryCell == nil {
+            tableView.registerNib(UINib.init(nibName: UserProfileViewController.USER_REPOSITORY_CELL_IDENTIFIER, bundle: nil), forCellReuseIdentifier: UserProfileViewController.USER_REPOSITORY_CELL_IDENTIFIER)
+            userRepositoryCell = tableView.dequeueReusableCellWithIdentifier(UserProfileViewController.USER_REPOSITORY_CELL_IDENTIFIER) as? UserRepositoryCell
+        }
+        return userRepositoryCell!
     }
     
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if let userProfileCell = cell as? UserProfileCell {
+            userProfileCell.showProfile(presenter.user.profile!)
+        } else if let userRepositoryCell = cell as? UserRepositoryCell {
+            userRepositoryCell.showRepository(presenter.user.repositories![indexPath.row - 1])
+        }
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        if let profile = presenter.user.profile where indexPath.row == 0 {
+            userProfileCell.showProfile(profile)
+            userProfileCell.bounds = CGRectMake(0, 0, CGRectGetWidth(tableView.bounds), CGRectGetHeight(userProfileCell.bounds))
+            userProfileCell.setNeedsLayout()
+            userProfileCell.layoutIfNeeded()
+            let size = userProfileCell.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            return size.height
+        } else if let repositories = presenter.user.repositories where indexPath.row > 0 {
+            userRepositoryCell.showRepository(repositories[indexPath.row - 1])
+            userRepositoryCell.bounds = CGRectMake(0, 0, CGRectGetWidth(tableView.bounds), CGRectGetHeight(userRepositoryCell.bounds))
+            userRepositoryCell.setNeedsLayout()
+            userRepositoryCell.layoutIfNeeded()
+            let size = userRepositoryCell.systemLayoutSizeFittingSize(UILayoutFittingCompressedSize)
+            return size.height
+        }
+        return 0
+    }
 }
